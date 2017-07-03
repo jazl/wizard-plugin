1207a3f35b3999ca15feb291f1b630022e583ce6
Alan Laya <alan_laya@hotmail.com>
2017-07-03 14:24:02 -0700

    action that launches wizard in static class

---
+++ src/com/azl/xwizard/ActionXStatic.java
@@ -0,0 +1,23 @@
+package com.azl.xwizard;
+
+import com.intellij.ide.wizard.AbstractWizardStepEx;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jazl on 7/3/17.
+ */
+public class ActionXStatic extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getProject();
+
+        Messages.showInfoMessage("About to launch using static class...","Info");
+        _AbstractExWizardStatic.show(project);
+    }
+}
---
+++ src/com/azl/xwizard/_AbstractExWizardStatic.java
@@ -0,0 +1,24 @@
+package com.azl.xwizard;
+
+import com.intellij.ide.wizard.AbstractWizardStepEx;
+import com.intellij.openapi.project.Project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by jazl on 7/3/17.
+ */
+public class _AbstractExWizardStatic {
+
+    public static void show(Project project) {
+        List<AbstractWizardStepEx> steps = new ArrayList<>();
+        steps.add(new StepAx("Step A"));
+        steps.add(new StepBx("Step B"));
+        steps.add(new StepCx("Step C"));
+
+        _AbstractExWizard wizard = new _AbstractExWizard("AbstractEx Wizard", project, steps);
+        wizard.show();
+
+    }
+}
