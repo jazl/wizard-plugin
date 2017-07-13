848700ae0788b86d561998bea0630ed6b841718c
jazlemail-velociraptor <jazlemail-velociraptor@yahoo.com>
2017-07-13 13:35:41 -0700

    working version of JavaFx WebView

--- src/com/azl/gui/ComponentsDialog.java
+++ src/com/azl/gui/ComponentsDialog.java
@@ -5,21 +5,37 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.concurrent.Worker;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import oracle.jrockit.jfr.JFR;
 import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Document;
 
+import javax.print.Doc;
 import javax.swing.*;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkListener;
+import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.HTMLFrameHyperlinkEvent;
 import java.awt.*;
+import java.awt.Event;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import javafx.concurrent.Worker.State;
+import org.w3c.dom.Element;
+import org.w3c.dom.events.*;
 
 public class ComponentsDialog extends JFrame {
 
@@ -87,6 +103,7 @@ public class ComponentsDialog extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JEditorPaneDialog d = new JEditorPaneDialog(frameRef, "With JEditorPane!");
+                d.loadPage();
                 d.setVisible(true);
             }
         });
@@ -95,6 +112,9 @@ public class ComponentsDialog extends JFrame {
     }
 
     private class WebViewDialog extends DialogWrapper {
+
+        WebView webView;
+        WebEngine webEngine;
 
         protected WebViewDialog(@Nullable Project project, boolean canBeParent) {
             super(project, canBeParent);
@@ -108,19 +128,53 @@ public class ComponentsDialog extends JFrame {
             final JFXPanel jfxPanel = new JFXPanel();
             //jfxPanel.setSize(new Dimension(300,300));
             jfxPanel.setPreferredSize(new Dimension(300,450));
-            final String content = "<html><body><h1>Hello</h1></body></html>";
+            String html = "<html><body><h3>Oh hai there!</h3><a href=\"http://www.microsoft.com\">Link</a><input id=\"btnSubmit\" type=\"submit\"/></body></html>";
 
             Platform.runLater(new Runnable() {
                 @Override
                 public void run() {
-                    WebView webView = new WebView();
+                    webView = new WebView();
+                    webEngine = webView.getEngine();
                     jfxPanel.setScene(new Scene(webView));
-                    webView.getEngine().loadContent(content);
+                    //webEngine.executeScript("alert('hello!');");
+                    webEngine.load(url);
+                    //webEngine.loadContent(html);
+
+                    webEngine.getLoadWorker().stateProperty().addListener(new ChangeListener<State>() {
+                        @Override
+                        public void changed(ObservableValue<? extends State> observable, State oldValue, State newValue) {
+                            System.out.println(observable.toString());
+                            if(observable.getValue() == State.SUCCEEDED) {
+                                System.out.println("READY!");
+                                setupDocumentListener();
+                            }
+                        }
+                    });
+
                 }
             });
 
             pack();
             return jfxPanel;
+        }
+
+        private void setupDocumentListener() {
+            EventListener listener = new EventListener() {
+                @Override
+                public void handleEvent(org.w3c.dom.events.Event evt) {
+                    EventTarget currentTarget = evt.getCurrentTarget();
+                    EventTarget target = evt.getTarget();
+                    Document document = webEngine.getDocument();
+                    Element errorMessageLabel = document.getElementById("errorMessageLabel");
+                    String errorMsg = errorMessageLabel.getTextContent();
+                    Platform.exit();
+                }
+            };
+
+            Document document = webEngine.getDocument();
+            Element el = document.getElementById("loginButton");
+            ((EventTarget) el).addEventListener("click", listener, false);
+
         }
     }
 
@@ -143,42 +197,62 @@ public class ComponentsDialog extends JFrame {
     }
 
     private class JEditorPaneDialog extends JDialog {
-
+        JEditorPane jEditorPane;
         public JEditorPaneDialog(Frame owner, String title) {
             super(owner, title);
-
             setSize(500, 500);
 
-            JEditorPane jEditorPane = new JEditorPane();
-            jEditorPane.setEditable(false);
+            jEditorPane = new JEditorPane();
+            //jEditorPane.setEditable(false);
 
             //JTextPane textPane = new JTextPane();
 
+            //jEditorPane.add(textPane);
+            add(jEditorPane);
+        }
+
+        public void loadPage() {
             try {
-                jEditorPane.setPage(url);
-            } catch (IOException e) {
+                System.out.println("Loading page from url: "+url);
+                jEditorPane.setContentType("text/html");
+                jEditorPane.setEditable(false);
+                ((HTMLEditorKit)jEditorPane.getEditorKitForContentType("text/html")).setAutoFormSubmission(false);
+                String html = "<html><body><h3>Oh hai there!</h3><a href=\"http://www.microsoft.com\">Link</a><input type=\"submit\"/></body></html>";
+                //jEditorPane.setPage(html);
+                jEditorPane.setText(html);
+                jEditorPane.add(new JButton("Hello!"));
+            } catch (Exception e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(this,"Got error: "+e.getMessage());
             }
-            jEditorPane.setContentType("text/html");
-            jEditorPane.setEditable(false);
-            ((HTMLEditorKit)jEditorPane.getEditorKitForContentType("text/html")).setAutoFormSubmission(false);
+
             jEditorPane.addHyperlinkListener(new HyperlinkListener()
             {
                 @Override
-                public void hyperlinkUpdate(HyperlinkEvent e)
-                {
-                    // this method will be called when user hits submit button on the form
+                public void hyperlinkUpdate(HyperlinkEvent e) {
+                    if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+                        JEditorPane pane = (JEditorPane) e.getSource();
+                        if (e instanceof HTMLFrameHyperlinkEvent) {
+                            HTMLFrameHyperlinkEvent evt = (HTMLFrameHyperlinkEvent) e;
+                            HTMLDocument doc = (HTMLDocument) pane.getDocument();
+                            doc.processHTMLFrameHyperlinkEvent(evt);
+                        } else {
+                            try {
+                                pane.setPage(e.getURL());
+                            } catch (Throwable t) {
+                                t.printStackTrace();
+                            }
+                        }
+                    }
                 }
             });
 
-            //jEditorPane.add(textPane);
-            add(jEditorPane);
         }
     }
 
     public static void main(String[] args) {
         ComponentsDialog dlg = new ComponentsDialog();
+        dlg.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         dlg.setVisible(true);
     }
 }
