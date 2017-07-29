25351448b4fb64825b657498b8d5265cf30b962c
jazl <alan_laya@hotmail.com>
2017-07-28 17:25:01 -0700

    add CheckBoxTree to a panel

--- src/com/azl/panels/PanelA.java
+++ src/com/azl/panels/PanelA.java
@@ -1,6 +1,7 @@
 package com.azl.panels;
 
 import com.azl.custom.DialogWithExtraButton;
+import com.azl.gui.MainFrame;
 import com.intellij.execution.filters.TextConsoleBuilderFactory;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.ConsoleViewContentType;
@@ -11,8 +12,12 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.TextBrowseFolderListener;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.CheckboxTree;
+import com.intellij.ui.CheckedTreeNode;
+import com.intellij.ui.treeStructure.Tree;
 
 import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,6 +31,7 @@ public class PanelA extends JPanel {
     private boolean showWebview = true;
     private Project project;
     private boolean doNotGoNext = false;
+    private CheckboxTree cbt;
 
     public PanelA() {
         final FileChooserDescriptor fcd = new FileChooserDescriptor(true,true,true,true,true,true);
@@ -90,6 +96,17 @@ public class PanelA extends JPanel {
             }
         });
 
+        add(createCheckBoxTreePanel());
+
+        JButton btnShowSelected = new JButton("Show selected");
+        btnShowSelected.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PanelA.this.showSelected();
+            }
+        });
+        add(btnShowSelected);
+
         setSize(new Dimension(500,500));
     }
     public void setProject(Project project) {
@@ -108,5 +125,94 @@ public class PanelA extends JPanel {
 
     public JComponent getFocusComponent() {
         return chkDoNotGo;
+    }
+
+    private static class AppRenderer extends CheckboxTree.CheckboxTreeCellRenderer {
+        private AppRenderer() {
+            super(true, true);
+        }
+
+        @Override
+        public void customizeRenderer(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+            getTextRenderer().append(value.toString());
+        }
+    }
+
+    private JPanel createCheckBoxTreePanel() {
+        JPanel panel = new JPanel();
+
+        com.intellij.ui.CheckBoxList<Object> cbl = new com.intellij.ui.CheckBoxList<Object>();
+        cbl.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        com.intellij.ui.CollectionListModel<Object> model = new com.intellij.ui.CollectionListModel<Object>();
+        model.add(new JCheckBox("Sarah"));
+        model.add(new JCheckBox("Sharon"));
+        model.add(new JCheckBox("Lindsay"));
+        model.add(new JCheckBox("Lyubov"));
+        model.add(new JCheckBox("Mary"));
+        model.add(new JCheckBox("Veronica"));
+        model.add(new JCheckBox("Becca"));
+        model.add(new JCheckBox("Erica"));
+        model.add(new JCheckBox("Tina"));
+        cbl.setModel(model);
+
+        //create the root node
+        DefaultMutableTreeNode root = new DefaultMutableTreeNode("Root");
+
+        //create the child nodes
+        DefaultMutableTreeNode vegetableNode = new DefaultMutableTreeNode("Vegetables");
+        vegetableNode.add(new DefaultMutableTreeNode("Capsicum"));
+        vegetableNode.add(new DefaultMutableTreeNode("Carrot"));
+        vegetableNode.add(new DefaultMutableTreeNode("Tomato"));
+        vegetableNode.add(new DefaultMutableTreeNode("Potato"));
+
+        DefaultMutableTreeNode fruitNode = new DefaultMutableTreeNode("Fruits");
+        fruitNode.add(new DefaultMutableTreeNode("Banana"));
+        fruitNode.add(new DefaultMutableTreeNode("Mango"));
+        fruitNode.add(new DefaultMutableTreeNode("Apple"));
+        fruitNode.add(new DefaultMutableTreeNode("Grapes"));
+        fruitNode.add(new DefaultMutableTreeNode("Orange"));
+
+        DefaultMutableTreeNode girlsNode = new DefaultMutableTreeNode("Girls");
+        girlsNode.add(new DefaultMutableTreeNode("Sharon"));
+        girlsNode.add(new DefaultMutableTreeNode("Sarah"));
+        girlsNode.add(new DefaultMutableTreeNode("Mary"));
+
+        //add the child nodes to the root node
+        root.add(vegetableNode);
+        root.add(fruitNode);
+        root.add(girlsNode);
+
+        //create the tree by passing in the root node
+        Tree tree = new Tree(root);
+
+        CheckedTreeNode r2 = new CheckedTreeNode("Root");
+        r2.setParent(null);
+        r2.setAllowsChildren(true);
+
+        CheckedTreeNode g2 = new CheckedTreeNode("Girls");
+        g2.add(new CheckedTreeNode("Lindsay"));
+        g2.add(new CheckedTreeNode("Sharon"));
+        g2.add(new CheckedTreeNode("Sarah"));
+        g2.add(new CheckedTreeNode("Mary"));
+
+        CheckedTreeNode b1 = new CheckedTreeNode("Boys");
+        b1.add(new CheckedTreeNode("Chuck"));
+        b1.add(new CheckedTreeNode("Elliott"));
+
+        r2.add(g2);
+        r2.add(b1);
+
+        cbt = new CheckboxTree(new PanelA.AppRenderer(), r2);
+
+        panel.setLayout(new BorderLayout());
+        panel.add(cbt, BorderLayout.CENTER);
+
+        return panel;
+    }
+
+    public void showSelected() {
+        CheckedTreeNode[] selectedNodes = cbt.getSelectedNodes(CheckedTreeNode.class, null);
+        System.out.println("selectedNodes? "+selectedNodes.length);
     }
 }
